8aa52a369fc3f5d657e66ea4278b697b650b3675
MartonCsizmazia <csmarton93@gmail.com>
2020-06-24 18:45:07 +0200

    test class created

---
+++ src/test/java/com/codecool/erpspringboot2/repository/AllRepositoryTest.java
@@ -0,0 +1,13 @@
+package com.codecool.erpspringboot2.repository;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class AllRepositoryTest {
+
+}
